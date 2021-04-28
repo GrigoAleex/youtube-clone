eb9b2f550a4b2bdd54e4dd100146cf22540b634d
GrigoAleex <grigorescu.aleex@gmail.com>
2021-04-28 23:00:44 +0300

    Setup project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      YOUTUBE 📺
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDKmXdRWTDkXdIyjpewZfUR7JvkPcoyMik",
+    authDomain: "clone-ae956.firebaseapp.com",
+    projectId: "clone-ae956",
+    storageBucket: "clone-ae956.appspot.com",
+    messagingSenderId: "29193121537",
+    appId: "1:29193121537:web:e80868a2f51c8a76647500",
+    measurementId: "G-C3C95MXJXY"
+  };
